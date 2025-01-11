a041b89602d83234b3a645305b07848b39f03c2c
Fernando <asialer05@hotmail.com>
2025-01-11 15:06:40 -0500

    proyecto base

---
+++ src/main/java/com/fernanando/ms/posts/app/twitter_clone_posts_service/TwitterClonePostsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.fernanando.ms.posts.app.twitter_clone_posts_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterClonePostsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitterClonePostsServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fernanando/ms/posts/app/twitter_clone_posts_service/TwitterClonePostsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fernanando.ms.posts.app.twitter_clone_posts_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitterClonePostsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
